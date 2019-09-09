ecf99f233bdfb30208771fae984550e3434b6ed4
Ashwani Tripathi <ashwani9807@gmail.com>
2019-09-08 20:00:23 -0400

    updated to include login flow

--- src/main/java/test/selenium/LoginFlow.java
+++ src/main/java/test/selenium/LoginFlow.java
@@ -1,6 +1,10 @@
 package test.selenium;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginFlow {
@@ -13,29 +17,66 @@ public class LoginFlow {
 		
 		WebDriver driver = new ChromeDriver();
 		
-		String baseUrl = "http://demo.guru99.com/test/newtours/";
-        String expectedTitle = "Welcome: Mercury Tours";
-        String actualTitle = "";
+		//String baseUrl = "http://demo.guru99.com/test/newtours/";
+		String baseUrl = "http://demo.guru99.com/";
+		String expectedSiteName = "Demo Site";
+		
+        //String expectedTitle = "Welcome: Mercury Tours";
+        //String actualTitle = "";
         
        // launch Fire fox and direct it to the Base URL
         driver.get(baseUrl);
 
         // get the actual value of the title
-        actualTitle = driver.getTitle();
+        //actualTitle = driver.getTitle();
 
+        String siteName = driver.findElement(By.id("site-name")).findElement(By.tagName("a")).getText();
+       // System.out.println("siteName is "+siteName);
         /*
          * compare the actual title of the page with the expected one and print
          * the result as "Passed" or "Failed"
          */
-        if (actualTitle.contentEquals(expectedTitle)){
-            System.out.println("Test Passed!");
+        if (siteName.contentEquals(expectedSiteName)){
+            System.out.println("Test Passed for site name");
         } else {
-            System.out.println("Test Failed");
+            System.out.println("Test Failed for site name");
         }
-       
-        //close Fire fox
-        driver.close();
         
+        //validate email address and submit fields
+         boolean isEmailFieldDisplayed = driver.findElement(By.name("emailid")).isDisplayed();
+         assertMessage(isEmailFieldDisplayed, "Email");
+       
+         boolean isSubmitButtonDisplayed = driver.findElement(By.name("btnLogin")).isDisplayed();
+         assertMessage(isSubmitButtonDisplayed, "Submit");
+         
+         //Enter Email address
+         driver.findElement(By.name("emailid")).sendKeys("testSelenium@mail.com");
+         
+         String populatedValueOfEmail = driver.findElement(By.name("emailid")).getText();
+         
+         //Click Submit
+         driver.findElement(By.name("btnLogin")).click();
+         
+         //Validate the next page with accpage fields as User ID : and Password :
+         List<WebElement> loggedInFields = driver.findElements(By.className("accpage"));
+         
+         boolean isUserIdDisplayed = "User ID :".equals(loggedInFields.get(0).getText());
+         assertMessage(isUserIdDisplayed, "User Id");
+         
+         boolean isPasswordDisplayed = "Password :".equals(loggedInFields.get(1).getText());
+         assertMessage(isPasswordDisplayed, "Password");
+         
+         
+        //close Fire fox
+        driver.close();  
+	}
+
+	private static void assertMessage(boolean isPresent, String field) {
+		if(isPresent) {
+        	 System.out.println("Test Passed for "+field);
+         }else {
+        	 System.out.println("Test Failed for "+field);
+         }
 	}
 
 }
